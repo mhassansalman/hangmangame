778443eadb705006d1468e8197fc345450536bdb
Muhammad Hassan Salman <mhassanscareer@gmail.com>
2026-05-13 23:17:01 +0500

    lists of words to guess from
    list of words to guess from

---
+++ lists.py
@@ -0,0 +1,3 @@
+list_veg = ["potato","tomato","cabbage","peas","radish","corn","capsicum","beetroot","carrot","spinach"]
+list_fruit = ["mango","banana","guava","berries","orange","watermelon","grapes","kiwi","apple","peach"]
+list_sports = ["basketball","tennis","cricket","hockey","badminton","football"]
